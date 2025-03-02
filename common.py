12fe9ee84fce3b8df0ba909d2691731362baa07c
Bhupesh Joshi <130831575+bhupeshcoding@users.noreply.github.com>
2025-03-02 18:37:11 +0530

    Update common.py

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 # Write a python program to find out common letters between two strings
-#corner case: Hello and hello ,common -{e,l,o} ,h and H are treated as different.
+#corner case: Hello and hello ,common -{e,l,o} ,h and H are treated as different, space will be also counted.
 def common():
   str1 = input("Enter 1st String")
   str2 = input("Enter 2nd String")
